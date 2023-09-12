981e04b85d95a6f435e79a2166701ddd90230bf7
yigit.turali <yigit.turali@data-boss.com.tr>
2023-09-12 16:29:44 +0300

    v2

--- Data_Preparation.py
+++ Data_Preparation.py
@@ -12,6 +12,30 @@ from sklearn.preprocessing import MinMaxScaler
 warnings.filterwarnings("ignore")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # if gpu is available use gpu
 
+
+def fft_denoiser(x, n_components, to_real=True):
+
+    n = len(x)
+
+    # compute the fft
+    fft = np.fft.fft(x, n)
+
+    # compute power spectrum density
+    # squared magnitud of each fft coefficient
+    PSD = fft * np.conj(fft) / n
+
+    # keep high frequencies
+    _mask = PSD > n_components
+    fft = _mask * fft
+
+    # inverse fourier transform
+    clean_data = np.fft.ifft(fft)
+
+    if to_real:
+        clean_data = clean_data.real
+
+    return clean_data
+
 def data_transfer(mat_file):
     del_list = []
     for key in mat_file.keys():
@@ -42,49 +66,61 @@ def create_dataset(sequences):
 
     return dataset, seq_len, n_features
 
+def create_sequences(X,y,seq_length):
+    xs, ys = [], []
+    for i in range(len(X) - seq_length):
+        x = X[i:(i+seq_length)]
+        y = y[i+seq_length]
+        xs.append(x)
+        ys.append(y)
+    return np.array(xs), np.array(ys)
 
 # Load the data
 current_working_directory = os.getcwd()
 data_path = os.path.join(current_working_directory, 'raw_data')
+i = 0
+for file in os.listdir(data_path):
+    i += 1
+    if file.endswith(".mat") and not file.endswith(".csv"):
+        data_dict = {}
+        data_raw = data_transfer(sio.loadmat(f'{data_path}/{file}'))
+        [data_raw.pop(key, None) for key in ["first","last"]]
+        data_dict[file] = pd.DataFrame(data_raw)
 
-# rs_nc3_mat = sio.loadmat(f'{data_path}/RS_NC3.mat')
-# s_nc3_mat = sio.loadmat(f'{data_path}/S_NC3.mat')
-s_nc2_mat = sio.loadmat(f'{data_path}/S_NC2.mat')
-s_nc2_labels = sio.loadmat(f'{data_path}/FirstLast_NC2.mat')
-# Transfer the data
-# rs_nc3_dataframe = pd.DataFrame(data_transfer(rs_nc3_mat))
-# s_nc3_dataframe = pd.DataFrame(data_transfer(s_nc3_mat))
-s_nc2_dataframe = pd.DataFrame(data_transfer(s_nc2_mat))
-s_nc2_labels_dataframe = pd.DataFrame(data_transfer(s_nc2_labels))
-s_nc2_dataframe = event_finder(s_nc2_dataframe, s_nc2_labels_dataframe)
-time_event = s_nc2_dataframe[["Event"]]
-data_transformer = MinMaxScaler()
-s_nc2_dataframe = pd.DataFrame(data_transformer.fit_transform(s_nc2_dataframe),
-                               columns=data_transformer.get_feature_names_out().tolist())
+        data_raw = data_transfer(sio.loadmat(f'{data_path}/{file}'))
+        data_dict[f"{file}_labels"] = pd.DataFrame(np.array([data_raw["first"],data_raw["first"]]).reshape(-1,2),columns=["first","last"])
+        data_dict[file] = event_finder(data_dict[file], data_dict[f"{file}_labels"])
+        time_event = data_dict[file][["Event"]]
+        data_dict[file].drop(columns=["timeS", "Event"], inplace=True)
+        data_dict[file][["Event"]] = time_event
 
-s_nc2_dataframe["amplitudeuWS_fourier"] = np.fft.fft(s_nc2_dataframe["amplitudeuWS"])
-s_nc2_dataframe["phaseuWS_fourier"] = np.fft.fft(s_nc2_dataframe["phaseuWS"])
-s_nc2_dataframe["ampCoulter_N_fourier"] = np.fft.fft(s_nc2_dataframe["ampCoulter_N"])
-s_nc2_dataframe.drop(columns=["timeS", "Event"], inplace=True)
-s_nc2_dataframe[["Event"]] = time_event
+        num_rows_per_file = 30000
+        num_files = len(data_dict[file]) // num_rows_per_file + 1
+        for j in range(num_files):
+            start_index = i * num_rows_per_file
+            end_index = (i + 1) * num_rows_per_file
 
-window_size = 2000
-step_size = 500
-# Create smaller Series using rolling windows
-smaller_series_list = []
-for start in range(0, len(s_nc2_dataframe) - window_size + 1, step_size):
-    end = start + window_size
-    window_series = s_nc2_dataframe.iloc[start:end].copy()
-    smaller_series_list.append(window_series)
+            # Slice the data for the current file
+            subset_data = data_dict[file].iloc[start_index:end_index]
+            subset_labels = data_dict[f"{file}_labels"].iloc[start_index:end_index]
 
-RANDOM_SEED = 42
-train_df, val_df = train_test_split(smaller_series_list, test_size=0.15, random_state=RANDOM_SEED, shuffle=True)
+            # Save the subset to a new CSV file
+            subset_data.to_csv(f'{data_path}/{i}_part_{j}.csv', index=False)
+            print(f"Process {i}:{j} is done")
 
-# splitting val dataframe to val & test
-val_df, test_df = train_test_split(val_df, test_size=0.5, random_state=RANDOM_SEED, shuffle=True)
+all_files = [os.path.join(data_path, file) for file in os.listdir(data_path) if file.endswith('.csv')]
+list_of_dfs = [pd.read_csv(file) for file in all_files]
+data_train = pd.concat(list_of_dfs, axis=0, ignore_index=True)
+y = data_train["Event"]
+X = data_train.drop(columns=["Event"])
 
-train_dataset, seq_len, n_features = create_dataset(train_df)
-val_dataset, seq_len, n_features = create_dataset(val_df)
-test_dataset, seq_len, n_features = create_dataset(test_df)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+scaler = MinMaxScaler(feature_range=(0, 1))
 
-print()
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+seq_length = 4000  # Choose your desired sequence length
+X_train, y_train = create_sequences(X_train, y_train, seq_length)
+X_test, y_test = create_sequences(X_test, y_test, seq_length)
+
